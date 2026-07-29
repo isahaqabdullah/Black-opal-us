5202a494dd3b988d0b4f8a0ce5b54d917a07e6b8
Isahaq Abdullah <isahaqabdullah@gmail.com>
2026-07-29 09:17:39 +0530

    Allow localhost preview origin

--- next.config.mjs
+++ next.config.mjs
@@ -56,6 +56,7 @@ const env = Object.fromEntries(
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  allowedDevOrigins: ['127.0.0.1'],
   env,
   async headers() {
     if (process.env.NODE_ENV === 'production') {
